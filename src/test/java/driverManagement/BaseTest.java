9d694d1a486b8111b4b7c5e6cf9194d19fb85a69
kkallu2 <64024175+kkallu2@users.noreply.github.com>
2023-12-03 11:59:08 +0530

    Update BaseTest.java

--- src/test/java/driverManagement/BaseTest.java
+++ src/test/java/driverManagement/BaseTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.locators.RelativeLocator;
 
