7ca942136f9d27a13ed01d61c40bf8dc91cad4b2
kkallu2 <64024175+kkallu2@users.noreply.github.com>
2023-12-03 11:15:41 +0530

    Update BaseTest.java

--- src/test/java/driverManagement/BaseTest.java
+++ src/test/java/driverManagement/BaseTest.java
@@ -28,7 +28,7 @@ public class BaseTest {
 		ChromeOptions options = new ChromeOptions();
 		 options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless=new");
+        options.addArguments("headless");
 
          driver = new ChromeDriver(options);
 	}
